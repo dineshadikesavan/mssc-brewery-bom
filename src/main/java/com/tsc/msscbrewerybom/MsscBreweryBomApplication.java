a112e73f2f7d988650003bd4a52d911061537a50
Dinesh A <dinesh.adikesavan@gmail.com>
2024-12-31 22:00:13 -0600

    initial commit

---
+++ src/main/java/com/tsc/msscbrewerybom/MsscBreweryBomApplication.java
@@ -0,0 +1,13 @@
+package com.tsc.msscbrewerybom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsc/msscbrewerybom/MsscBreweryBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsc.msscbrewerybom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
